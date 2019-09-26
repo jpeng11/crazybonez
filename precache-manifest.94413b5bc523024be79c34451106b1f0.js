d5d1963dc6c264d230b400cde81dadbd1f1ca347
Michael Stuhlman <msstuhlman@gmail.com>
2019-09-26 17:45:52 -0500

    Updates

---
+++ precache-manifest.94413b5bc523024be79c34451106b1f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a99420564054e25aa4d4f927eecef424",
+    "url": "/crazybonez/index.html"
+  },
+  {
+    "revision": "3e0147c9e18af9b5524f",
+    "url": "/crazybonez/static/css/main.f800e8fe.chunk.css"
+  },
+  {
+    "revision": "ffe3d4ab9c8d86bb0c83",
+    "url": "/crazybonez/static/js/2.a2c9aa90.chunk.js"
+  },
+  {
+    "revision": "3e0147c9e18af9b5524f",
+    "url": "/crazybonez/static/js/main.77537cfb.chunk.js"
+  },
+  {
+    "revision": "4e676fc6aaaae212d5a3",
+    "url": "/crazybonez/static/js/runtime-main.773d7b96.js"
+  },
+  {
+    "revision": "aa90582b23a8c39a2655e47dffdd9c53",
+    "url": "/crazybonez/static/media/crazybonezhead.aa90582b.png"
+  }
+]);
